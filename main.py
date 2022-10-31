0327414bef4b7b5d604b04d1bb39069f3998876e
Behic G <bguven@na.edu>
2022-10-30 23:25:34 -0500

    Add files via upload
    Day 1 game - generate a band name

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("What is your name? \n")
+
+print("Welcome, " + name + "! \nThis program will help you find your band name using python. ")
+
+print("\n Let's get started!")
+
+city_name = input("\n Which city did you grow up?  ")
+
+pet_name = input("\n What was your first pet's name?  ")
+
+band_name = city_name + " " + pet_name
+
+print("\n Perfect! Our program has generated the following band name for you: " + band_name)
