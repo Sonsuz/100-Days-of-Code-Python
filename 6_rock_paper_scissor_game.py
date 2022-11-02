73ea9af6ee7b4ad651d799e931e03ad72fe02b6d
Behic G <bguven@na.edu>
2022-11-02 14:57:48 -0500

    Update 6_rock_paper_scissor_game.py

--- 6_rock_paper_scissor_game.py
+++ 6_rock_paper_scissor_game.py
@@ -5,3 +5,17 @@ import random
 user_input = int(input("Let's play rocks, papers, scissors. 0 for 🪨, 1 for 📄. 2 for ✂️ \n\n"))
 
 r = random.randint(0, 2)
+
+if r == 0:
+  computer_choice = "rock"
+elif r == 1:
+  computer_choice = "paper"
+else:
+  computer_choice = "scissor"
+
+if user_input == 0:
+  user_choice = "rock"
+elif user_input == 1:
+  user_choice = "paper"
+else:
+  user_choice = "scissor"
